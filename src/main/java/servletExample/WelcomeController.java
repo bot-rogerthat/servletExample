e2d60b1d360f6ba680c1623308da8ea86b12678c
Sergej Yakushenok <sergej_yakushenok@epam.com>
2016-06-15 16:03:55 +0300

    first impl

---
+++ src/main/java/servletExample/WelcomeController.java
@@ -0,0 +1,29 @@
+package servletExample;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession(false);
+        String login = (String) session.getAttribute("login");
+        String name = (String) session.getAttribute("name");
+        if (login != null && name != null) {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("WelcomeController " + login + " : " + name);
+        } else {
+            response.sendRedirect("index.html");
+        }
+    }
+}
